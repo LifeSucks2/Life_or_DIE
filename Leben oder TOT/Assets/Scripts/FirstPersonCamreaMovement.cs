bfc27bbb8252e109997f3cf19c259549f709c6f9
LifeSucks2 <56684387+LifeSucks2@users.noreply.github.com>
2020-11-21 20:44:39 +0100

    Update from Ali

---
+++ Leben oder TOT/Assets/Scripts/FirstPersonCamreaMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamreaMovement : MonoBehaviour
+{
+    public float mouseSensivity = 100f;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.Rotate(-Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensivity, 0, 0);
+      Player.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensivity, 0);
+    }
+}
---
+++ Leben oder TOT/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizantal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(horizantal, 0, vertical);
+    }
+}
