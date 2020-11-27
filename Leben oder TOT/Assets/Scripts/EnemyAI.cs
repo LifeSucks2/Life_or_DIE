64a14d8a2edbe34280d21beadabcb0efa13e39f3
LifeSucks2 <56684387+LifeSucks2@users.noreply.github.com>
2020-11-27 16:28:57 +0100

    EnemyAI and Player optimizing
    Enemy folgt wenn er Player sieht, stoppt und geht zurüch bis er nicht zu nah dran ist

---
+++ Leben oder TOT/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private float speed = 5f;
+    public float seeingDistance;
+    public float shootingDistance;
+    public float timeBtwShots;
+    public float startTimeBtwShots;
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;    
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Vector3.Distance(transform.position, player.position) >= seeingDistance) {
+            transform.position = this.transform.position;
+            print(Vector3.Distance(transform.position, player.position));
+        }
+        else if(Vector3.Distance(transform.position, player.position) > shootingDistance && Vector3.Distance(transform.position, player.position) <= seeingDistance) {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        } 
+        else if(Vector3.Distance(transform.position, player.position) == shootingDistance) {
+            transform.position = this.transform.position;
+        }
+        else if(Vector3.Distance(transform.position, player.position) < shootingDistance) {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+    }
+}
