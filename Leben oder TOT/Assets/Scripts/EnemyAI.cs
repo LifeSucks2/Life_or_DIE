d336621205e839427e7c6a00359afa07fbb52ac1
Hakan <hakankanak@hotmail.de>
2020-11-30 00:53:39 +0100

    rotate

--- Leben oder TOT/Assets/Scripts/EnemyAI.cs
+++ Leben oder TOT/Assets/Scripts/EnemyAI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    private float speed = 5f;
+    private float speed = 10f;
     public float seeingDistance;
     public float shootingDistance;
     public float timeBtwShots;
@@ -13,23 +13,30 @@ public class EnemyAI : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
-        player = GameObject.FindGameObjectWithTag("Player").transform;    
+        //player = GameObject.FindGameObjectWithTag("Player").transform;    
+        player = GameObject.FindWithTag("Player").transform; //Fand ich nur schöner
     }
 
     // Update is called once per frame
     void Update() {
+
+        
+
         if(Vector3.Distance(transform.position, player.position) >= seeingDistance) {
             transform.position = this.transform.position;
             print(Vector3.Distance(transform.position, player.position));
         }
         else if(Vector3.Distance(transform.position, player.position) > shootingDistance && Vector3.Distance(transform.position, player.position) <= seeingDistance) {
             transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            transform.LookAt(player); //Rotate to Player
         } 
         else if(Vector3.Distance(transform.position, player.position) == shootingDistance) {
             transform.position = this.transform.position;
+            transform.LookAt(player); //Rotate to Player
         }
         else if(Vector3.Distance(transform.position, player.position) < shootingDistance) {
             transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            transform.LookAt(player); //Rotate to Player
         }
     }
 }
