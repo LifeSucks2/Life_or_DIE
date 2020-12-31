2ac8e17eef1562f0778daea056db306348a4b84e
Hakan <hakankanak@hotmail.de>
2020-12-31 16:45:00 +0100

    Tutorial Level

---
+++ Leben oder TOT/Assets/FirstRoomTextChange.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FirstRoomTextChange : MonoBehaviour
+{
+    public Text myText;
+    private bool w = true, a = true, s = true, d = true, space = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //welcome Screen
+        myText.text = "Press W to Move Foreward";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Press A to change the Text
+        if (w == true)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                myText.text = "Press S to Backwards";
+                w = false;
+            }
+        }
+        //Press W to change the Text
+        if (s == true)
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                myText.text = "Press A to Left";
+                s = false;
+            }
+        }
+        //Press A to change the Text
+        if (a == true)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                myText.text = "Press D to Right";
+                a = false;
+            }
+        }
+        //Press A to change the Text
+        if (d == true)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                myText.text = "Press Space to Jump";
+                d = false;
+            }
+        }
+
+        //Press A to change the Text
+        if (space == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                myText.text = "Good Job";
+                space = false;
+            }
+        }
+    }
+}
--- Leben oder TOT/Assets/Scripts/Gun.cs
+++ Leben oder TOT/Assets/Scripts/Gun.cs
@@ -1,35 +1,35 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
-public class Gun : MonoBehaviour
-{
-    public float damage = 10f;
-    public float range = 100f;
+//public class Gun : MonoBehaviour
+//{
+//    public float damage = 10f;
+//    public float range = 100f;
 
-    public Camera fpsCam;
+//    public Camera fpsCam;
 
 
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))// Left click
-        {
-            Shoot();
-        }
-    }
+//    void Update()
+//    {
+//        if (Input.GetButtonDown("Fire1"))// Left click
+//        {
+//            Shoot();
+//        }
+//    }
 
-    void Shoot()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
+//    void Shoot()
+//    {
+//        RaycastHit hit;
+//        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+//        {
+//            Debug.Log(hit.transform.name);
 
-            Enemy target = hit.transform.GetComponent<Enemy>();
+//            Enemy target = hit.transform.GetComponent<Enemy>();
 
-            if (target != null) // If the component is an enemy 
-            {
-                target.TakeDamage(damage);
-            }
-        }
+//            if (target != null) // If the component is an enemy 
+//            {
+//                target.TakeDamage(damage);
+//            }
+//        }
 
-    }
-}
+//    }
+//}
