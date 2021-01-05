ac555fc71245005fe3b8c9f2b6021ab30aca0c02
LifeSucks2 <56684387+LifeSucks2@users.noreply.github.com>
2021-01-05 19:12:29 +0100

    Enemy things

--- Leben oder TOT/Assets/Scripts/Enemy.cs
+++ Leben oder TOT/Assets/Scripts/Enemy.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public GameObject door;
+
+    public const float maxHealth = 800f;
     public PlayerVitals pv;
-    public float health = 50f;
+    public float health = 800f;
     [SerializeField] private AudioClip damageSound;
+    public GameObject healthBarUI;
+    public Slider slider;
+    public void Start(){
+        slider.value = maxHealth;
+    }
     public void TakeDamage(float amount)
     {
+        healthBarUI.SetActive(true);
         if (health <= 0f)
         {
             Die();
@@ -18,12 +27,15 @@ public class Enemy : MonoBehaviour
             gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Camera.main.transform.position, -0.1f);
         }
         health -= amount;
-        AudioSource.PlayClipAtPoint(damageSound, Camera.main.transform.position, 1000f);  
+        AudioSource.PlayClipAtPoint(damageSound, Camera.main.transform.position, 1000f);
+        slider.value = health;
     }
 
     void Die()
     {
         Destroy(gameObject);
         Destroy(door);
+        Destroy(healthBarUI);
+        Destroy(slider);
     }
 }
