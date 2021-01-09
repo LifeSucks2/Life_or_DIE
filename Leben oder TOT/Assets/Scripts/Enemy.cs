9ed6deaf3ba2ab0c5a78a6ddc4374f6c84e8882f
Hakan <hakankanak@hotmail.de>
2021-01-09 19:53:52 +0100

    Enemy hit Effect

--- Leben oder TOT/Assets/Scripts/Enemy.cs
+++ Leben oder TOT/Assets/Scripts/Enemy.cs
@@ -12,19 +12,27 @@ public class Enemy : MonoBehaviour
     [SerializeField] private AudioClip damageSound;
     public GameObject healthBarUI;
     public Slider slider;
+    public ParticleSystem DieEffect = null;
+
     public void Start(){
         slider.value = maxHealth;
     }
+
+ 
     public void TakeDamage(float amount)
     {
+
+        
         healthBarUI.SetActive(true);
         if (health <= 0f)
         {
+            
             Die();
             pv.points += 100;
         }
         else if(gameObject.transform.tag != "Detect Area"){
             gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Camera.main.transform.position, -0.1f);
+                DieEffect.Play() ;
         }
         health -= amount;
         AudioSource.PlayClipAtPoint(damageSound, Camera.main.transform.position, 1000f);
