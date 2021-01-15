87242c2966ccbb6603a385996f9efd1f1a489333
LifeSucks2 <56684387+LifeSucks2@users.noreply.github.com>
2021-01-15 01:22:57 +0100

    Antitoxin und Healt Packs

---
+++ Leben oder TOT/Assets/Scripts/PickUps.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUps : MonoBehaviour
+{
+    [SerializeField] PlayerVitals pv;
+    [SerializeField] AudioClip pickupSound;
+
+    void OnTriggerEnter(Collider other){
+        if(pv.health < pv.maxHealth){
+            if(other.tag == "Bottle_Health"){
+                if(pv.health > 190f){
+                    pv.health = pv.maxHealth;
+                }
+                else{
+                    pv.health += 10f;
+                }
+            AudioSource.PlayClipAtPoint(pickupSound, Camera.main.transform.position);
+            Destroy(other.gameObject);
+            }
+        }
+        if(pv.poisen > 0f) {
+            if(other.tag == "Bottle_Antitoxin"){
+                pv.poisen -= 0.1f;
+                AudioSource.PlayClipAtPoint(pickupSound, Camera.main.transform.position);
+                Destroy(other.gameObject);
+                if(pv.poisen < 0){
+                    pv.poisen = 0;
+                }
+            
+            }
+        }   
+   }
+}
--- Leben oder TOT/Assets/Scripts/PlayerVitals.cs
+++ Leben oder TOT/Assets/Scripts/PlayerVitals.cs
@@ -6,8 +6,11 @@ using UnityEngine.SceneManagement;
 public class PlayerVitals : MonoBehaviour
 {
     public int points = 0;
-    public float health = 50f;
+    public float poisen = 0;
+    public float health = 200;
+    public float maxHealth = 200;
     public Slider playerHealth;
+    public Slider Toxin;
     public MenuScript ms;
 
     void start()
@@ -15,9 +18,31 @@ public class PlayerVitals : MonoBehaviour
         playerHealth.minValue = 0f;
         playerHealth.maxValue = health;
         playerHealth.value = health;
+
+        Toxin.minValue = 0f;
+        Toxin.maxValue = 1f;
+        Toxin.value = poisen;
     }
 
-   
+    void Update(){
+        poisen += 0.0001f;
+        /*if(poisen == 0.1f){
+            print("hurra");
+            health -= 15f;
+        }*/
+        
+
+        if(health <= maxHealth){
+            playerHealth.value = health;
+        }
+        if(poisen <= 1f){
+            Toxin.value = poisen;
+        }
+        else{
+            ms.callME();
+        }
+
+    }
     public void TakeDamage(float amount)
     {
         health -= amount;
